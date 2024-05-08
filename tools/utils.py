cbb420120336aecbd4488882f4afc577fa338700
Sirui Li <slray@nyu.edu>
2024-05-08 19:35:24 +0400

    Adding comments, pulished the functions

--- tools/utils.py
+++ tools/utils.py
@@ -1,32 +1,34 @@
 # Author: ray
 # Date: 5/6/24
 # Description:
+from typing import List, Dict, Set
 
 from pylab import *
 
 
 def iterate_solver(sol, fun, xstart, tstart, h, tend):
-    """Iterate a one-step solver with time step h starting from initial
-condition xstart at tstart, until tend is reached.
+    """
+        Iterate a one-step solver with time step h starting from initial
+        condition xstart at tstart, until tend is reached.
 
-Inputs
-sol:    a function that implements a one-step solver. sol(f,x,t,h) must
-        yield an estimate of x(t+h)
-fun:    the vector field that defines the (system) of ordinary differential
-        equation(s). The time derivative of the state x must be given by f(x,t)
-xstart: the initial condition, a vector of n elements, or a float for 1D
-        problems.
-tstart: the time corresponding to the initial condition. It's ignored for
-        autonomous problems (that is, problems where f does not explicitly
-        depend on time).
-tend:   the time after which the iterations must stop.
+        Inputs
+        sol:    a function that implements a one-step solver. sol(f,x,t,h) must
+                yield an estimate of x(t+h)
+        fun:    the vector field that defines the (system) of ordinary differential
+                equation(s). The time derivative of the state x must be given by f(x,t)
+        xstart: the initial condition, a vector of n elements, or a float for 1D
+                problems.
+        tstart: the time corresponding to the initial condition. It's ignored for
+                autonomous problems (that is, problems where f does not explicitly
+                depend on time).
+        tend:   the time after which the iterations must stop.
 
-Returns
-xs:    an (m+1)xn matrix containing the state vectors of the solution.
-       xs[0,:] is equal to the initial condition xstart. m is the number of
-       time steps performed. n is the number of elements of xstart.
-times: the times correspoding to the vectors in xs. times[0] is tstart
-"""
+        Returns
+        xs:    an (m+1)xn matrix containing the state vectors of the solution.
+               xs[0,:] is equal to the initial condition xstart. m is the number of
+               time steps performed. n is the number of elements of xstart.
+        times: the times correspoding to the vectors in xs. times[0] is tstart
+    """
     xs = [xstart]
     times = [tstart]
     iter = 0
@@ -40,6 +42,19 @@ times: the times correspoding to the vectors in xs. times[0] is tstart
 
 
 def Runge_Kutta(f, x, t, h):
+    """
+    Runge-Kutta method
+    Parameters
+    ----------
+    f
+    x
+    t
+    h
+
+    Returns
+    -------
+
+    """
     s_1 = f(x, t)
     s_2 = f(x + h / 2 * s_1, t + h / 2)
     s_3 = f(x + h / 2 * s_2, t + h / 2)
@@ -47,9 +62,25 @@ def Runge_Kutta(f, x, t, h):
     return x + h / 6 * (s_1 + 2 * s_2 + 2 * s_3 + s_4)
 
 
-def tree_to_layers(tree, queue, max_layer: int):
+def tree_to_layers(tree: Dict[Set], queue: list, max_layer: int) -> List[List]:
+    """
+    Takes a tree and converts it into a tree of layers.
+    Parameters
+    ----------
+    tree : dict
+        The tree to convert.
+    queue : list
+        The queue of layers to convert. Queue of tuples (node, layer_index)
+    max_layer : int
+        The maximum number of layers to convert.
+
+    Returns
+    -------
+    list
+        List of layers. Each layer is a list.
+    """
     layers = []
-    # queue = [("root", 0)]  # Queue of tuples (node, layer_index)
+    # queue = [("root", 0)]
 
     prev_layer = None
     while queue and max_layer > 0:
